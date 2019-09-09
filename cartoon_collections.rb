ceee8c37ef3fb097f51d4115c0f6426d167f13f0
Kenneth Neylon <kenneth_neylon@hotmail.com>
2019-09-09 06:11:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,14 +9,15 @@ def summon_captain_planet(array)
   end  
 end
 
-def long_planeteer_calls(array)
-  long = false
-  array.each do |call|
+def long_planteer_calls(long_planteer_calls)# code an argument here
+  # Your code here
+  answer = false
+  long_planteer_calls.each do |call|
     if call.length > 4
       answer = true
-    end 
-  end  
-  answer  
+    end
+  end
+    answer
 end
 
 def find_the_cheese# code an argument here
