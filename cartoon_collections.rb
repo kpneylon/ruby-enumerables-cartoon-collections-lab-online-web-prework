40260d141f34f32288aab570384ce09f12df8e8c
Kenneth Neylon <kenneth_neylon@hotmail.com>
2019-09-09 06:16:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ def long_planeteer_calls(planeteer)
     answer
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
